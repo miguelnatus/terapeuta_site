e6b728fa98316c3ba2893c7d8f4936f97ecf34e2
Miguel Natus <miguelnatus@gmail.com>
2025-12-18 12:50:27 -0300

    novo

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
 import { jsxLocPlugin } from "@builder.io/vite-plugin-jsx-loc";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
 import react from "@vitejs/plugin-react";
 import fs from "node:fs";
 import path from "path";
 import { defineConfig } from "vite";
 import { vitePluginManusRuntime } from "vite-plugin-manus-runtime";
 
+
 const plugins = [react(), tailwindcss(), jsxLocPlugin(), vitePluginManusRuntime()];
 
 export default defineConfig({
-  plugins,
+  plugins: [
+    react(),
+    tailwindcss(), // <--- Adicione isso
+  ],
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
